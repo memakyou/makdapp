c2297408cb6af1a90f4ac1c3cdcc762cc4c84540
MEMAKYOU <114414123+memakyou@users.noreply.github.com>
2023-06-16 20:21:54 +0100

    minor updates

--- components/MusicPlayer/Playhead.tsx
+++ components/MusicPlayer/Playhead.tsx
@@ -131,16 +131,12 @@ const Artwork = styled.img`
 }
 `;
 
-const Artwork2 = styled.img<{ isFullscreen: boolean; mouseX: number; mouseY: number }>`
-  width: 90%;
-  // height: 800px;
+const Artwork2 = styled.img<{ isFullscreen: boolean; }>`
+  width: 100%;
   border-radius: 0px;
   transition: transform 0.5s;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-
-  transform: ${({ isFullscreen, mouseX, mouseY }) =>
-    isFullscreen ? `translate(-${mouseX}px, -${mouseY}px)` : 'none'};
 
   &:hover {
     background-color: #ccc002;
@@ -170,7 +166,7 @@ const CardContainer = styled.div`
   gap: 2rem;
   border: none; /* Add this line */
   transition: border 0.3s; /* Add this line */
-  cursor: pointer;
+  // cursor: pointer;
 
   @media (min-width: 768px) {
     flex-direction: row;
@@ -222,11 +218,6 @@ const PercentageBox = styled.div`
   font-size: 12px;
 `;
 
-const PercentageBox1 = styled.div`
-  color: #141a20;
-  // text-align: center;
-  font-size: 12px;
-`;
 
 const ModalBackgroundImage = styled.img`
   position: absolute;
@@ -244,7 +235,7 @@ const FullscreenOverlay = styled.div<{ isFullscreen: boolean }>`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.9);
+  background: #d4d4d4;
   display: ${({ isFullscreen }) => (isFullscreen ? 'flex' : 'none')};
   align-items: center;
   justify-content: center;
@@ -294,7 +285,6 @@ const Playhead: React.FC<PlayheadProps> = ({
   const [isFullscreen, setIsFullscreen] = useState(false);
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
-  
   const handleArtworkClick = () => {
     setIsFullscreen(!isFullscreen);
   };
@@ -329,29 +319,8 @@ const Playhead: React.FC<PlayheadProps> = ({
     };
   }, []);
 
-  const [rotateX, setRotateX] = useState(0);
-  const [rotateY, setRotateY] = useState(0);
-
   
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const { clientX, clientY } = e;
-      const { innerWidth, innerHeight } = window;
-
-      const dx = (clientX - innerWidth / 2) / 20; // change the divisor for more/less sensitivity
-      const dy = (clientY - innerHeight / 2) / 20; // change the divisor for more/less sensitivity
-
-      setRotateX(dy);
-      setRotateY(dx);
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
+  
   return (
     <>
       <PlayheadContainer >
@@ -389,10 +358,11 @@ const Playhead: React.FC<PlayheadProps> = ({
           {/* Content of the fullscreen overlay */}
           {currentSong && (
             <Artwork2
-              src={currentSong.trackArt}
-              alt="Fullscreen Artwork"
-              onClick={handleArtworkClick}
-            />
+            src={currentSong.trackArt}
+            alt="Fullscreen Artwork"
+            onClick={handleArtworkClick}
+            isFullscreen={isFullscreen}
+          />
           )}
         </FullscreenOverlay>
         <div>
